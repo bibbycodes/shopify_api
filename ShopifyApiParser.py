53f6a1d4527a852303e54531800a481e97c4a51a
Robert <bibbycodes@gmail.com>
2019-12-07 18:04:53 +0000

    refactors into class

--- ShopifyApiParser.py
+++ ShopifyApiParser.py
@@ -4,41 +4,22 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
+api_key = os.getenv("API_KEY")
+api_pass = os.getenv("API_PASS")
+
 class ShopifyApiParser:
   def __init__(self, api_key, api_pass):
     self.api_key = api_key
     self.api_pass = api_pass
     self.base_url = "https://{}:{}@themoflo.myshopify.com/admin/api/2019-10/{}.json"
 
-  def parse_orders(self):
+  def parse_all_orders(self, array_of_orders):
+    parsed_orders = []
     for order in array_of_orders:
-      line_items = order["line_items"][0]
-      
-      parsed = {
-        "order_id": order["id"],
-        "email" : order["email"],
-        "phone_number" : order["phone"],
-        "created_at" : order["created_at"],
-        "updated_at" : order["updated_at"],
-        "processed_at" : order["processed_at"],
-        "order_number" : order["order_number"],
-        "total_price" : order["total_price"],
-        "taxes_included" : order["taxes_included"],
-        "quantity" : line_items["quantity"],
-        "confirmed" : order["confirmed"],
-        "checkout_token" : order["checkout_token"],
-        "vendor" : line_items["title"],
-        "ticket_type" : line_items["variant_title"],
-        "address" : line_items["origin_location"],
-        "billing_details" : order["billing_address"],
-        "client_details" : order["client_details"],
-        "fulfillment_status" : order["fulfillment_status"],
-        }
+      parsed_orders.append(self.parse_order(order))
 
-      if ("payment_details" in order):
-        parsed["payment_details"] = order["payment_details"]
+    return parsed_orders
 
-      return parsed
 
   def fetch_endpoint(self, endpoint):
     url = self.base_url.format(self.api_key, self.api_pass, endpoint)
@@ -76,3 +57,11 @@ class ShopifyApiParser:
       parsed["payment_details"] = order["payment_details"]
 
     return parsed
+
+MoFLo = ShopifyApiParser(api_key, api_pass)
+unparserd_orders = MoFLo.fetch_endpoint("orders")
+parsed_orders = MoFLo.parse_all_orders(unparserd_orders)
+
+for order in parsed_orders:
+  print(order)
+  print("\n")
